f9102bde3576ddbfa1f7857badc0f9132b929bb1
ngh75 <planet_ff@hotmail.fr>
2017-03-25 13:25:03 +0100

    inserer le porojet calculatrice

---
+++ cal/src/Modele.java
@@ -0,0 +1,16 @@
+
+
+public class Modele  {
+	private static final long serialVersionUID = 1L;
+	String value="0";
+	public Modele(){}
+	public String getValue(){
+		return value;
+	}
+	public void setValue(String value){
+		this.value=value;
+	}
+	public double getDouble(){
+		return Double.valueOf(value).doubleValue();
+	}
+}
