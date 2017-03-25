5b4f035d19f5312c8414692d06d8976b66c64d26
ngh75 <planet_ff@hotmail.fr>
2017-03-25 18:04:58 +0100

    ajouter Bonjouejava

---
+++ Bonjour.java
@@ -0,0 +1,10 @@
+public  class Bonjour {  
+	 public  static  void main (String [] arguments) {  
+		System.out.println("Bonjour le monde 1") ;  
+		System.out.println("Bonjour le monde 2") ; 
+		System.out.println("Bonjour le monde 3") ; 
+		System.out.println("Bonjour le monde 4") ; 
+		System.out.println("Bonjour le monde 5") ; 
+		  
+	}
+}
